daf73f92a245300a34e846939b261c5903d8a525
scalablebrands <akil.kurji@gmail.com>
2018-09-27 21:35:00 -0500

    ak's js201 excercises 01 & 02

--- exercises/01-hello-world.js
+++ exercises/01-hello-world.js
@@ -2,9 +2,24 @@
 // string that says hello to the name.
 // Example: hello('Mustache') should return 'Hello, Mustache!'
 
+function hello (name) {
+    return 'Hello, ' + name + '!'
+}
 
 
+// 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "helloDefault" such that if no name is given it will return
 // 'Hello, world!'
 // Otherwise it behaves the same as the "hello" function.
+
+function helloDefault (name){
+    if (!name) {
+       return "Hello, world!"
+    }
+    else {
+        return 'Hello, ' + name + '!'
+    }
+}
+
+
